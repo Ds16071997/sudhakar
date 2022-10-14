e128250a36fca90dde1df5cd4fbe1048ec774222
sudhakar <donthamsettisudhakar@gmail.com>
2022-10-14 19:40:00 +0530

    added changes

---
+++ main.tf
@@ -0,0 +1,3 @@
+resource "aws_s3_bucket" "d" {
+  bucket = "albert-einsten"
+  }
---
+++ provide.tf
@@ -0,0 +1,5 @@
+provider "aws" {
+  region     = "us-west-1"
+  access_key = "AKIA4R46WLF3P3YGPSU5"
+  secret_key = "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"
+}
